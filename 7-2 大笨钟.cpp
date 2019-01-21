179e2ab7f171e8a483a77bb422960a0ed5a2d1dc
intBJWWW <38395031+intbjw@users.noreply.github.com>
2019-01-21 09:57:59 +0800

    Add files via upload

---
+++ 7-2 大笨钟.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int hh,mm;
+	scanf("%d:%d",&hh,&mm);
+	if((hh>=0&&hh<=11||hh==12&&mm==0))
+        printf("Only %02d:%02d.  Too early to Dang.",hh,mm);
+    else{
+        {
+            for(int i=0;i<hh-12;i++)
+                printf("Dang");
+            if(mm!=0)
+                printf("Dang");
+            printf("\n");
+        }
+    }
+	//printf("%d,%d",hh,mm);
+	return 0;
+}
